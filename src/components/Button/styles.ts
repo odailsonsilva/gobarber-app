0facee6c112141921157bfd06cf108e2a415e7dc
odailsonsilva <odafreelancerti@gmail.com>
2020-11-11 08:20:35 -0300

    ESTRUTURA INICIAL APP GOBARBER

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components/native';
+import { RectButton } from 'react-native-gesture-handler'; // btn mais indicados para cross plataform
+
+export const Container = styled(RectButton)`
+  width: 100%;
+  height: 60px;
+  background: #ff9000;
+  border-radius: 10px;
+
+  justify-content: center;
+  align-items: center;
+
+  margin-top: 8px;
+`;
+
+export const ButtonText = styled.Text`
+  font-size: 'RobotoSlab-Medium';
+  color: #312e38;
+  font-size: 18px;
+`;
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,17 +1,42 @@
 import React from 'react';
 import { Image } from 'react-native';
-
-import { Container, Title } from './styles';
+import Icon from 'react-native-vector-icons/Feather';
 
 import LogoImg from '../../assets/logo.png';
+import Button from '../../components/Button';
+import Input from '../../components/Input';
+
+import {
+  Container,
+  Title,
+  ForgotPassword,
+  ForgotPasswordText,
+  CreateAccountButton,
+  CreateAccountButtonText,
+} from './styles';
 
 const SignIn: React.FC = () => {
   return (
-    <Container>
-      <Image source={LogoImg} />
+    <>
+      <Container>
+        <Image source={LogoImg} />
 
-      <Title>Faça seu logon</Title>
-    </Container>
+        <Title>Faça seu logon</Title>
+
+        <Input name="email" icon="mail" placeholder="E-mail" />
+        <Input name="password" icon="lock" placeholder="Senha" />
+        <Button onPress={() => {}}>Entrar</Button>
+
+        <ForgotPassword onPress={() => {}}>
+          <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+        </ForgotPassword>
+      </Container>
+
+      <CreateAccountButton onPress={() => {}}>
+        <Icon name="log-in" size={20} color="#ff9000" />
+        <CreateAccountButtonText>Criar uma conta</CreateAccountButtonText>
+      </CreateAccountButton>
+    </>
   );
 };
 
