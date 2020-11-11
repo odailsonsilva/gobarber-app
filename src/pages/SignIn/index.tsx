6f1c1108ef8c644265787a1eb30f1ce85a9571ad
odailsonsilva <odafreelancerti@gmail.com>
2020-11-11 15:47:35 -0300

    feat: Usabilidade dos formularios (Inputs)

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,6 +1,16 @@
-import React from 'react';
-import { Image } from 'react-native';
+import React, { useCallback, useRef } from 'react';
+import {
+  Image,
+  KeyboardAvoidingView,
+  Platform,
+  View,
+  ScrollView,
+  TextInput,
+} from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
+import { Form } from '@unform/mobile';
+import { FormHandles } from '@unform/core';
 
 import LogoImg from '../../assets/logo.png';
 import Button from '../../components/Button';
@@ -16,23 +26,72 @@ import {
 } from './styles';
 
 const SignIn: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+  const passwordInputRef = useRef<TextInput>(null);
+  const navigation = useNavigation();
+
+  const handleSignIn = useCallback((data: object) => {
+    console.log(data);
+  }, []);
+
   return (
     <>
-      <Container>
-        <Image source={LogoImg} />
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <ScrollView
+          contentContainerStyle={{ flex: 1 }}
+          keyboardShouldPersistTaps="handled"
+        >
+          <Container>
+            <Image source={LogoImg} />
+            <View>
+              <Title>Faça seu logon</Title>
+            </View>
 
-        <Title>Faça seu logon</Title>
+            <Form ref={formRef} onSubmit={handleSignIn}>
+              <Input
+                autoCorrect={false}
+                autoCapitalize="none"
+                keyboardType="email-address"
+                name="email"
+                icon="mail"
+                placeholder="E-mail"
+                returnKeyType="next"
+                onSubmitEditing={() => {
+                  passwordInputRef.current?.focus();
+                }}
+              />
 
-        <Input name="email" icon="mail" placeholder="E-mail" />
-        <Input name="password" icon="lock" placeholder="Senha" />
-        <Button onPress={() => {}}>Entrar</Button>
+              <Input
+                ref={passwordInputRef}
+                name="password"
+                icon="lock"
+                placeholder="Senha"
+                secureTextEntry
+                returnKeyType="send"
+                onSubmitEditing={() => {
+                  formRef.current?.submitForm();
+                }}
+              />
+              <Button
+                onPress={() => {
+                  formRef.current?.submitForm();
+                }}
+              >
+                Entrar
+              </Button>
+            </Form>
 
-        <ForgotPassword onPress={() => {}}>
-          <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
-        </ForgotPassword>
-      </Container>
-
-      <CreateAccountButton onPress={() => {}}>
+            <ForgotPassword onPress={() => {}}>
+              <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+            </ForgotPassword>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+      <CreateAccountButton onPress={() => navigation.navigate('SignUp')}>
         <Icon name="log-in" size={20} color="#ff9000" />
         <CreateAccountButtonText>Criar uma conta</CreateAccountButtonText>
       </CreateAccountButton>
--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -1,11 +1,13 @@
 import styled from 'styled-components/native';
+import { Platform } from 'react-native';
+import { getBottomSpace } from 'react-native-iphone-x-helper';
 
 export const Container = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
 
-  padding: 0 30px;
+  padding: 0 30px ${Platform.OS === 'android' ? 150 : 40}px;
 `;
 
 export const Title = styled.Text`
@@ -33,11 +35,16 @@ export const CreateAccountButton = styled.TouchableOpacity`
   background: #312e38;
   border-top-width: 1px;
   border-color: #232129;
-  padding: 16px 0 16px;
+  padding: 16px 0 ${16 + getBottomSpace()}px;
 
   justify-content: center;
   align-items: center;
   flex-direction: row;
 `;
 
-export const CreateAccountButtonText = styled.Text``;
+export const CreateAccountButtonText = styled.Text`
+  color: #ff9000;
+  font-size: 18px;
+  font-family: 'Roboto-Regular';
+  margin-left: 16px;
+`;
